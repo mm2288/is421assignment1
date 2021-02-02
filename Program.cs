fc75cbe8b7b59939480df81b6ebe7265fb4d7724
mm2288 <mm2288@njit.edu>
2021-02-02 16:08:45 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace is421assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Megan!");
+
+            Console.WriteLine("Press ESC key to exit command line.");
+
+            var key = Console.ReadKey();
+
+            if (key.Key == ConsoleKey.Escape)
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
